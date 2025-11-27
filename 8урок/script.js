8ea8411e53bfd7527665e322a38fddffdd0bfea6
Bauyrzhan <gogicom@mail.ru>
2025-11-27 22:20:24 +0500

    добавил нескрлько папок

---
+++ 8урок/script.js
@@ -0,0 +1,30 @@
+console.log("сегодня 8й урок");
+
+//var variable1 = 'сегодня урок по var'//
+let variable2 = 'сегодня урок по let'
+const varriable3 = "Сегодня урок про const"
+
+
+console.log(variable2);
+console.log(varriable3);
+
+const num1 = 5;
+const num2 = 10;
+
+const sum = num1 + num2
+
+console.log(sum)
+
+
+//типы данных//
+const str = 'строка'
+const num = 2.2
+const bool = true
+const arr = [1, 2, 3, 4, 5]
+const = arr2 = ['test,test2']
+const obj = {
+    test:'Строка из ключа test из объекта obj'
+}
+const empty = null
+
+
